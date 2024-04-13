35b76aeaf3d0a606d557746a501a1815efeba8a1
Rost07062 <72071956+Rost07062@users.noreply.github.com>
2024-04-13 17:34:40 +0300

    Update Rost.py

--- Rost.py
+++ Rost.py
@@ -1,10 +1,48 @@
 import pandas as pd
-def change(x):
+import numpy as np
+
+def changerecl(x):
     if x <= 1:
         return 'мало'
     if x == 2:
         return 'средне'
     if x > 2:
         return 'много'
+    
+def changeage(x):
+    if x <= 44:
+        return 'молодой возраст'
+    if 45 <= x <= 59:
+        return 'средний возраст'
+    if x >= 60:
+        return 'пожилой возраст'
+def standr_comf_rh(df):
+    if 40 <= df['rh'] <= 60:
+        return 'OK'
+    else:
+        return 'not OK'
+    
 df = pd.read_csv('data.csv', delimiter=';') 
-df['reclam_cat'] = df['количество_рекламаций'].map(change)
+df['countmen'] = 1
+
+df['реклам_категория'] = df['количество_рекламаций'].map(changerecl)
+df['возрастная_категория'] = df['возраст'].map(changeage)
+
+dfmeanage = df[['пол', 'страна', 'возраст']].groupby(['пол', 'страна']).mean().round().astype(int)
+
+goodtemp = df.loc[df['предпочтительное_изменение_температуры'] == 'Без изменений']
+
+dfcomftemp = goodtemp[['температура_воздуха_в_помещении', 'возрастная_категория']].groupby('возрастная_категория').mean()
+
+df3 =  goodtemp[['страна', 'пол', 'countmen']].groupby(['страна', 'пол']).count()
+df3.reset_index(inplace=True)
+for i in range(len(df3)):
+    df3['countmen'][i] = df3['countmen'][i] / len(df.loc[(df['страна'] == df3['страна'][i]) & (df['пол'] == df3['пол'][i])]) * 100
+    
+pivot = pd.pivot_table(df, index=['страна', 'пол', 'возрастная_категория'], values=['температура_воздуха_в_помещении', 'температура_воздуха_на_улице', 'rh'], 
+                       aggfunc='mean')
+
+df['стандр_комф_вл'] = df.apply(standr_comf_rh, axis=1)
+
+
+
