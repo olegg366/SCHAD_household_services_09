1c7ee250c15240aff5f608ad7280a85945ef2d2f
Rost07062 <72071956+Rost07062@users.noreply.github.com>
2024-04-15 23:34:11 +0300

    Update Rost.py

--- Rost.py
+++ Rost.py
@@ -1,6 +1,10 @@
+
 import pandas as pd
 import numpy as np
+from scipy.stats import spearmanr, shapiro, pearsonr, ttest_ind, mannwhitneyu, chi2_contingency
+from sklearn.preprocessing import LabelEncoder
 
+le = LabelEncoder()
 def changerecl(x):
     if x <= 1:
         return 'мало'
@@ -21,8 +25,13 @@ def standr_comf_rh(df):
         return 'OK'
     else:
         return 'not OK'
-    
-df = pd.read_csv('data.csv', delimiter=';') 
+def corrs(x1, x2):
+    ty1 = type(x1[0])
+    ty2 = type(x2[0])
+    if ty1 == ty2:
+        if ty1 == np.float64:
+            pass
+df = pd.read_csv('corrected_data.csv', delimiter=',') 
 df['countmen'] = 1
 
 df['реклам_категория'] = df['количество_рекламаций'].map(changerecl)
@@ -43,7 +52,64 @@ pivot = pd.pivot_table(df, index=['страна', 'пол', 'возрастна�
                        aggfunc='mean')
 
 df['стандр_комф_вл'] = df.apply(standr_comf_rh, axis=1)
+cols = list(df.columns)
+cols.remove('Unnamed: 0')
+for i in cols:
+    for j in cols[cols.index(i) + 1:]:
+        nulli = df[i].isnull().any()
+        nullj = df[j].isnull().any()
+        if not nulli and not nullj:
+            shapii = None
+            shapij = None
+            print(i, j)
+            print()
+            if type(df[i][0]) is np.float64:
+                shapii = shapiro(df[i])[1]
+            if type(df[j][0]) is np.float64:
+                shapij = shapiro(df[j])[1]
+            if (shapii is not None and shapij is None) or (shapii is None and shapij is not None):
+                
+                if shapii is not None:
+                    le1 = le.fit_transform(df[j])
+                    if shapii < 0.05:
+                        
+                        stu = ttest_ind(df[i], le1)
+                        print(f'corr: {stu.statistic}')
+                        print(f'pvalue: {stu.pvalue}')
+                    else:
+                        mann = mannwhitneyu(df[i], df[j])
+                        print(f'corr: {mann.statistic}')
+                        print(f'pvalue: {mann.pvalue}')
+                        
+                else:
+                    le2 = le.fit_transform(df[i])
+                    if shapij < 0.05:
+                        stu = ttest_ind(le2, df[j])
+                        print(f'corr: {stu.statistic}')
+                        print(f'pvalue: {stu.pvalue}')
+                    else:
+                        mann = mannwhitneyu(df[i], df[j])
+                        print(f'corr: {mann.statistic}')
+                        print(f'pvalue: {mann.pvalue}')
+                
+                        
+            if shapii is not None and shapij is not None:
+                if shapii < 0.05 or shapij < 0.05:
+                    pear = pearsonr(df[i], df[j])
+                    print(f'corr: {pear.statistic}')
+                    print(f'pvalue: {pear.pvalue}')
+                else:
+                    spear = spearmanr(df[i], df[j])
+                    print(f'corr: {spear.statistic}')
+                    print(f'pvalue: {spear.pvalue}')
+            if shapii is None and shapij is None:
+                crosschi = pd.crosstab(df[i], df[j])
+                chi = chi2_contingency(crosschi)
+                print(f'corr: {chi.statistic}')
+                print(f'pvalue: {chi.pvalue}')
+            print()
+            print()            
+                
+    
 
-
-#q123
-#123456
+df.to_csv('corrected_data_categor_byRost.csv')
